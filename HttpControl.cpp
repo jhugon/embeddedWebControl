63ca15f438555fb8414b2704a3eeb51384fb661e
Justin Hugon <jhugon@users.noreply.github.com>
2019-03-09 17:35:47 -0600

    Now use HttpControl

---
+++ HttpControl.cpp
@@ -0,0 +1,55 @@
+#include "HttpControl.h"
+
+ewc::HttpControl::HttpControl(NetworkInterface* network):
+    _httpServer(new HttpServer(network))
+{
+} // HttpControl constructor
+
+ewc::HttpControl::~HttpControl()
+{
+  delete _httpServer;
+} // HttpControl destructor
+
+
+void ewc::HttpControl::addVar(char* varName, void* varAddress, VarType varType, char* description)
+{
+} // addVar
+
+nsapi_error_t ewc::HttpControl::start(uint16_t port)
+{
+  return _httpServer->start(port,callback(this,&ewc::HttpControl::handle));
+} // start
+
+void ewc::HttpControl::handle(ParsedHttpRequest* request, TCPSocket* socket)
+{
+    http_method method = request->get_method();
+    std::string url = request->get_url();
+    if (url == "/")
+    {
+      if (method == HTTP_GET)
+      {
+        HttpResponseBuilder builder(200);
+        builder.set_header("Content-Type", "text/html; charset=utf-8");
+
+        char response[] = "<html><head><title>Hello from mbed</title></head>"
+            "<body>"
+                "<h1>mbed webserver</h1>"
+                "<button id=\"toggle\">Toggle LED</button>"
+                "<script>document.querySelector('#toggle').onclick = function() {"
+                    "var x = new XMLHttpRequest(); x.open('POST', '/toggle'); x.send();"
+                "}</script>"
+            "</body></html>";
+
+        builder.send(socket, response, sizeof(response) - 1);
+      } // if method == GET
+      else
+      {
+        HttpResponseBuilder builder(405); // method not allowed
+        builder.send(socket, NULL, 0);
+      } // else method isn't GET
+    } // if url == /
+    else {
+        HttpResponseBuilder builder(404); // not found
+        builder.send(socket, NULL, 0);
+    }
+} // handle
--- HttpControl.h
+++ HttpControl.h
@@ -1,6 +1,10 @@
 /** \brief Web server class for embedded web control
  */
 
+#include "mbed.h"
+#include "http_server.h"
+#include "http_response_builder.h"
+
 namespace ewc
 {
   enum VarType
@@ -11,7 +15,7 @@ namespace ewc
       OctInteger8,
       OctInteger16,
       OctInteger32,
-      OctInteger8,
+      HexInteger8,
       HexInteger16,
       HexInteger32,
       FixFloat32,
@@ -25,17 +29,26 @@ namespace ewc
 
   struct VarRecord
   {
-    char[40] varName;
+    char varName[40];
     void* varAddress;
     VarType varType;
-    char[100] description;
+    char description[100];
   };
 
   /** \brief Web server class for embedded web control
    */
-  class WebServer
+  class HttpControl
   {
     public:
+      /** \brief Create webserver using the given network stack
+       * 
+       *  You have to use the start method to start the server
+       *
+       */
+      HttpControl(NetworkInterface* network);
+
+      ~HttpControl();
+
       /** \brief Make a variable available to the web server
        *
        *  Maybe an array of these should be passed to the constructor, 
@@ -43,12 +56,16 @@ namespace ewc
        *
        *  Is it useful that I bounds check and put these in fixed length char arrays?
        */
-      addVar(char* varName, void* varAddress, VarType varType, char* description);
+      void addVar(char* varName, void* varAddress, VarType varType, char* description);
 
-      /** \brief Connect to TCP port
-       *
+      /** \brief Start the server
+ *
+       *  Start the server and connect it to its own port
+       *  this will start a new thread
        */
-      connectOrListen(port....);
+      nsapi_error_t start(uint16_t port);
+
+    private:
 
       /** \brief Handle request
        *
@@ -56,8 +73,10 @@ namespace ewc
        *  that to decide if to return a json string (REST)
        *  or some kind of status web page
        */
-      handle(request....);
-    private:
+      void handle(ParsedHttpRequest* request, TCPSocket* socket);
     
+      /////////////////////////////////
+
+      HttpServer* _httpServer;
   };
 }
