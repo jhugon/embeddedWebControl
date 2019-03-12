ec6e5ee87e5b62ca1c36c8c2e7707bcb9faf6873
Justin Hugon <jhugon@users.noreply.github.com>
2019-03-12 18:00:06 -0500

    Changes to make work with static IP

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "mbed.h"
+#include "EthernetInterface.h"
 #include "http_server.h"
 #include "http_response_builder.h"
 
@@ -42,11 +43,38 @@ int main() {
     pc.baud(115200);
 
     // Connect to the network (see mbed_app.json for the connectivity method used)
-    NetworkInterface* network = NetworkInterface::get_default_instance();
-    if (!network) {
-        printf("Cannot connect to the network, see serial output\n");
-        return 1;
-    }
+    //NetworkInterface* network = NetworkInterface::get_default_instance();
+    EthernetInterface* network = new EthernetInterface();
+    network->set_dhcp(false);
+    network->set_network("10.123.123.5","255.255.255.0","10.123.123.1");
+    debug("About to connect to network\n");
+    network->connect();
+    const nsapi_connection_status_t & connection_status = network->get_connection_status();
+    switch (connection_status)
+    {
+        case NSAPI_STATUS_LOCAL_UP:
+          debug("Connection is \"local up\"\n");
+          break;
+        case NSAPI_STATUS_GLOBAL_UP:
+          debug("Connection is \"global up\"\n");
+          break;
+        case NSAPI_STATUS_DISCONNECTED:
+          debug("Connection is \"disconnected\"\n");
+          return 1;
+          break;
+        case NSAPI_STATUS_CONNECTING:
+          debug("Connection is \"connecting\"\n");
+          return 1;
+          break;
+        case NSAPI_STATUS_ERROR_UNSUPPORTED:
+          debug("Connection is \"Error: Networking Unsupported\"\n");
+          return 1;
+          break;
+        default:
+          debug("Connection is in Unknown Error state\n");
+          return 1;
+          break;
+    } //switch
 
     HttpServer server(network);
     nsapi_error_t res = server.start(8080, &request_handler);
